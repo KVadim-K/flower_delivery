9ab53d378741b4d0b7215e3138d6c704677e0ca9
KVadim-K <kvadimk@yandex.ru>
2024-09-29 23:55:30 +0300

    createsuperuser
    Создание Суперпользователя

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status', 'order_date']
+    list_filter = ['status', 'order_date']
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'phone', 'address', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone', 'address')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
