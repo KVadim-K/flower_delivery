8fe12049d0a67469a1194b29f59129bc8f4e696d
KVadim-K <kvadimk@yandex.ru>
2024-10-07 23:39:33 +0300

    ADD css

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', views.create_order, name='create_order'),
+    path('create/<int:product_id>/', views.create_order, name='create_order'),
+    path('create/', views.create_order, name='create_order_no_id'),  # маршрут без product_id
     path('history/', views.order_history, name='order_history'),
     path('reorder/<int:order_id>/', views.reorder, name='reorder'),
 ]
