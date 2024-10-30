8b3265c119d4fe2bb3db7d39e01c4a9b9be71125
KVadim-K <kvadimk@yandex.ru>
2024-10-30 22:09:16 +0300

    Add a little
    Изменение формы регистрации

--- users/forms.py
+++ users/forms.py
@@ -16,34 +16,18 @@ class RegistrationForm(UserCreationForm):
         })
     )
     phone = forms.CharField(
-        required=False,
+        required=True,  # Сделаем обязательным, если необходимо
         label='Номер телефона',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'Введите номер телефона'
         })
     )
-    address = forms.CharField(
-        required=False,
-        label='Адрес',
-        widget=forms.Textarea(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите адрес',
-            'rows': 3
-        })
-    )
-    telegram_id = forms.CharField(
-        required=False,
-        label='Telegram ID',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите ваш Telegram ID'
-        })
-    )
+    # Удаляем поле 'address'
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'phone', 'address', 'telegram_id', 'password1', 'password2')
+        fields = ('username', 'email', 'phone', 'password1', 'password2')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -51,8 +35,4 @@ class RegistrationForm(UserCreationForm):
             raise forms.ValidationError('Пользователь с таким email уже существует.')
         return email
 
-    def clean_telegram_id(self):
-        telegram_id = self.cleaned_data.get('telegram_id')
-        if telegram_id and User.objects.filter(telegram_id=telegram_id).exists():
-            raise forms.ValidationError('Этот Telegram ID уже связан с другим пользователем.')
-        return telegram_id
+    # Удаляем метод clean_telegram_id, так как поле отсутствует в форме
